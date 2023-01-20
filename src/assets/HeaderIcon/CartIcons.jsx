637daffbd661846c8107fcdbf52a7e888d7cf9cf
digitlhaus-dev3 <development3@digitlhaus.com>
2023-01-20 18:48:53 +0530

    Header Component css Change

--- src/assets/HeaderIcon/CartIcons.jsx
+++ src/assets/HeaderIcon/CartIcons.jsx
@@ -1,13 +1,33 @@
-import React from "react";
+import React from 'react'
 
 const CartIcons = () => {
-    return (
-        <svg xmlns="http://www.w3.org/2000/svg" width="27" height="27" viewBox="0 0 27 27" fill="none">
-            <path d="M8.3252 15.975L6.6377 7.875H20.8127C21.6002 7.875 22.0502 8.55 21.9377 9.225L20.3627 16.0875C20.1377 16.9875 19.3502 17.6625 18.4502 17.775H10.8002C9.5627 17.8875 8.5502 17.1 8.3252 15.975Z" stroke="white" stroke-linecap="round" stroke-linejoin="round"/>
-            <path d="M6.6375 7.875L5.85 4.5H3.9375" stroke="white" stroke-linecap="round" stroke-linejoin="round"/>
-            <path d="M19.2371 20.5875C19.0121 20.5875 18.7871 20.8125 18.7871 21.0375C18.7871 21.2625 19.0121 21.4875 19.2371 21.4875C19.4621 21.4875 19.6871 21.2625 19.6871 21.0375C19.6871 20.7 19.4621 20.5875 19.2371 20.5875Z" stroke="white" stroke-linecap="round" stroke-linejoin="round"/>
-            <path d="M9.78789 20.5875C9.56289 20.5875 9.33789 20.8125 9.33789 21.0375C9.33789 21.2625 9.56289 21.4875 9.78789 21.4875C10.0129 21.4875 10.2379 21.2625 10.2379 21.0375C10.2379 20.7 10.0129 20.5875 9.78789 20.5875Z" stroke="white" stroke-linecap="round" stroke-linejoin="round"/>
-        </svg>
-    )
+  return (
+    <svg xmlns="http://www.w3.org/2000/svg" width="27" height="27" viewBox="0 0 27 27" fill="none">
+      <path
+        d="M8.3252 15.975L6.6377 7.875H20.8127C21.6002 7.875 22.0502 8.55 21.9377 9.225L20.3627 16.0875C20.1377 16.9875 19.3502 17.6625 18.4502 17.775H10.8002C9.5627 17.8875 8.5502 17.1 8.3252 15.975Z"
+        stroke="white"
+        stroke-linecap="round"
+        stroke-linejoin="round"
+      />
+      <path
+        d="M6.6375 7.875L5.85 4.5H3.9375"
+        stroke="white"
+        stroke-linecap="round"
+        stroke-linejoin="round"
+      />
+      <path
+        d="M19.2371 20.5875C19.0121 20.5875 18.7871 20.8125 18.7871 21.0375C18.7871 21.2625 19.0121 21.4875 19.2371 21.4875C19.4621 21.4875 19.6871 21.2625 19.6871 21.0375C19.6871 20.7 19.4621 20.5875 19.2371 20.5875Z"
+        stroke="white"
+        stroke-linecap="round"
+        stroke-linejoin="round"
+      />
+      <path
+        d="M9.78789 20.5875C9.56289 20.5875 9.33789 20.8125 9.33789 21.0375C9.33789 21.2625 9.56289 21.4875 9.78789 21.4875C10.0129 21.4875 10.2379 21.2625 10.2379 21.0375C10.2379 20.7 10.0129 20.5875 9.78789 20.5875Z"
+        stroke="white"
+        stroke-linecap="round"
+        stroke-linejoin="round"
+      />
+    </svg>
+  )
 }
-export default CartIcons;
+export default CartIcons
