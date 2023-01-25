b50a1f41bfc9277cba5f8deaaa77780b274ac805
digitlhaus-dev3 <development3@digitlhaus.com>
2023-01-25 20:33:33 +0530

    Updated code for menu and header services

--- src/sections/Header/index.jsx
+++ src/sections/Header/index.jsx
@@ -22,144 +22,125 @@
  * ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE
  * OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
  */
-import * as React from 'react'
-import { useRouter } from 'frontend-router'
-import { useCart } from 'frontend-checkout'
-import Link from 'Components/Link'
-import Badge from 'Components/Badge'
-import Container from 'Components/Container'
-import Grid from 'Components/Grid'
-import Logo from 'Components/Logo'
-import Menu from 'Components/Menu'
-import SearchQueryInput from 'Components/SearchQueryInput'
-import { useCustomerState } from 'frontend-customer'
-import IconButton from 'Components/IconButton'
-import HStack from 'Components/HStack'
-// import { AccountIcons, CartIcons, Volume, UsdIcons } from '../../components/Assets/index'
-import './styles.css'
-
-import { ACCOUNT_URL, ACCOUNT_LOGIN_URL } from 'Components/Data'
-
-/**
- * @typedef { import("lib/types").Media } Media
- * @typedef { import("lib/types").Menu } MenuType
- * @typedef {{
- *  logoImage?: Media
- *  menu?: MenuType
- *  sticky?: boolean
- * }} HeaderProps
- *
- * @param { HeaderProps } props
- */
-const Header = ({ logoImage, menu, sticky }) => {
-  const router = useRouter()
-  const { isLoggedIn } = useCustomerState()
-  // @ts-ignore
-  const [{ items = [] }, { showCart }] = useCart()
-
-  // @ts-ignore
-  const itemsQuantity = items.reduce((acc, currentItem) => acc + currentItem.quantity, 0)
-
-  const cartIconAriaLabel = !!itemsQuantity ? `Cart with ${itemsQuantity} items.` : 'Cart is empty.'
-
-  const handleSearchSubmit = React.useCallback(
-    (/** @type { string } */ query) => router.push(`/search?q=${query}`),
-    [router],
-  )
-
-  const menuLinks = menu && menu.menuLinks
-
-  return (
-    <Grid
-      as="header"
-      position={sticky ? 'sticky' : 'initial'}
-      top="0"
-      // templateAreas={{ base: "'menu logo cart'", md: "'logo menu cart'" }}
-      // templateColumns={{ base: '5rem 1fr 5rem', sm: '3rem 1fr 3rem', md: 'auto 1fr auto' }}
-      // alignItems="center"
-      // columnGap={{ base: 2, md: 10 }}
-      // px={{ base: 4, md: 8 }}
-      // py={4}
-      // bg="brand.100"
-      // w="100%"
-      zIndex="docked"
-    >
-      <div className="container">
-        <div className="header-main">
-          <div className="header-left">
-            <Container
-              className="header-icon"
-              gridArea="logo"
-              justifySelf={{ base: 'center', md: 'left' }}
-              w={['20', '32']}
-            >
-              <Logo image={logoImage} />
-            </Container>
-            {menu ? (
-              <Container gridArea="menu" className="nav-links">
-                <Menu
-                  content={
-                    <SearchQueryInput
-                      onClick={event => event.stopPropagation()}
-                      onKeyDown={event => event.stopPropagation()}
-                      onSearchSubmit={handleSearchSubmit}
-                    />
-                  }
-                  links={menuLinks}
-                />
-              </Container>
-            ) : null}
-          </div>
-          <div className="header-right">
-            <HStack gridArea="cart" justifySelf="right" spacing="4">
-              {/* <Container display={{ base: 'none', md: 'block' }}>
-                <SearchPopover onSearchSubmit={handleSearchSubmit} />
-              </Container> */}
-              <Link
-                href={isLoggedIn ? ACCOUNT_URL : ACCOUNT_LOGIN_URL}
-                title="Price"
-                aria-label="Price"
-                className="usdIcon"
-              >
-                <span>USD</span>
-              </Link>
-              {/* <IconButton
-                variant="iconWrapper"
-                aria-live="assertive"
-                aria-label={cartIconAriaLabel}
-                onClick={showCart}
-                size={8}
-                icon={
-                  itemsQuantity ? (
-                    <Badge badgeContent={itemsQuantity} variant="cart">
-                      {<CartIcons />}
-                    </Badge>
-                  ) : (
-                    <CartIcons />
-                  )
-                }
-              /> */}
-              {/* <Link
-                href={isLoggedIn ? ACCOUNT_URL : ACCOUNT_LOGIN_URL}
-                title="Navigate to account"
-                aria-label="Navigate to my account"
-              >
-                <AccountIcons aria-label="Navigate to my account" />
-              </Link>
-              <Link
-                href={isLoggedIn ? ACCOUNT_URL : ACCOUNT_LOGIN_URL}
-                title="Volume"
-                aria-label="controll volume"
-                className="volume"
-              >
-                <Volume />
-              </Link> */}
-            </HStack>
-          </div>
-        </div>
-      </div>
-    </Grid>
-  )
-}
-
-export default Header
+ import * as React from 'react'
+ import { useRouter } from 'frontend-router'
+ import { useCart } from 'frontend-checkout'
+ import { BiCart, BiUser } from 'react-icons/bi'
+ import Link from 'Components/Link'
+ import Badge from 'Components/Badge'
+ import Container from 'Components/Container'
+ import Grid from 'Components/Grid'
+ import Logo from 'Components/Logo'
+ import Menu from 'Components/Menu'
+ import SearchPopover from 'Components/SearchPopover'
+ import Icon from 'Components/Icon'
+ import SearchQueryInput from 'Components/SearchQueryInput'
+ import { useCustomerState } from 'frontend-customer'
+ import IconButton from 'Components/IconButton'
+ import HStack from 'Components/HStack'
+ import { ACCOUNT_URL, ACCOUNT_LOGIN_URL } from 'Components/Data'
+ 
+ const CartIcon = () => <Icon display="block" as={BiCart} size="md" />
+ 
+ /**
+  * @typedef { import("lib/types").Media } Media
+  * @typedef { import("lib/types").Menu } MenuType
+  * @typedef {{
+  *  logoImage?: Media
+  *  menu?: MenuType
+  *  sticky?: boolean
+  * }} HeaderProps
+  *
+  * @param { HeaderProps } props
+  */
+ const Header = ({ logoImage, menu, sticky }) => {
+   const router = useRouter()
+   const { isLoggedIn } = useCustomerState()
+   const [{ items = [] }, { showCart }] = useCart()
+ 
+   const itemsQuantity = items.reduce((acc, currentItem) => acc + currentItem.quantity, 0)
+ 
+   const cartIconAriaLabel = !!itemsQuantity ? `Cart with ${itemsQuantity} items.` : 'Cart is empty.'
+ 
+   const handleSearchSubmit = React.useCallback(
+     (/** @type { string } */ query) => router.push(`/search?q=${query}`),
+     [router],
+   )
+ 
+   const menuLinks = menu && menu.menuLinks
+ 
+   return (
+     <Grid
+       as="header"
+       position={sticky ? 'sticky' : 'initial'}
+       top="0"
+       templateAreas={{ base: "'menu logo cart'", md: "'logo menu cart'" }}
+       templateColumns={{ base: '5rem 1fr 5rem', sm: '3rem 1fr 3rem', md: 'auto 1fr auto' }}
+       alignItems="center"
+       columnGap={{ base: 2, md: 10 }}
+       px={{ base: 4, md: 8 }}
+       py={4}
+       bg="brand.100"
+       w="100%"
+       zIndex="docked"
+     >
+       <Container gridArea="logo" justifySelf={{ base: 'center', md: 'left' }} w={['20', '32']}>
+         <Logo image={logoImage} />
+       </Container>
+ 
+       {menu ? (
+         <Container gridArea="menu">
+           <Menu
+             content={
+               <SearchQueryInput
+                 onClick={event => event.stopPropagation()}
+                 onKeyDown={event => event.stopPropagation()}
+                 onSearchSubmit={handleSearchSubmit}
+               />
+             }
+             links={menuLinks}
+           />
+         </Container>
+       ) : null}
+ 
+       <HStack gridArea="cart" justifySelf="right" spacing="4">
+         <Container display={{ base: 'none', md: 'block' }}>
+           <SearchPopover onSearchSubmit={handleSearchSubmit} />
+         </Container>
+ 
+         <Link
+           href={isLoggedIn ? ACCOUNT_URL : ACCOUNT_LOGIN_URL}
+           title="Navigate to account"
+           aria-label="Navigate to my account"
+         >
+           <IconButton
+             variant="iconWrapper"
+             aria-label="Navigate to my account"
+             size={8}
+             icon={<Icon display="block" as={BiUser} size="md" />}
+           />
+         </Link>
+ 
+         <IconButton
+           variant="iconWrapper"
+           aria-live="assertive"
+           aria-label={cartIconAriaLabel}
+           onClick={showCart}
+           size={8}
+           icon={
+             itemsQuantity ? (
+               <Badge badgeContent={itemsQuantity} variant="cart">
+                 <CartIcon />
+               </Badge>
+             ) : (
+               <CartIcon />
+             )
+           }
+         />
+       </HStack>
+     </Grid>
+   )
+ }
+ 
+ export default Header
+ 
