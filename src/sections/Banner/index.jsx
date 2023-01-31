0b1a0fdf9ca4ec2c2b778bd1fe83b3719e84bac1
digitlhaus-dev3 <development3@digitlhaus.com>
2023-01-31 11:41:51 +0530

    Added React-slick slider

--- src/sections/Banner/index.jsx
+++ src/sections/Banner/index.jsx
@@ -1,26 +1,36 @@
 import React from 'react'
 import styles from './styles.module.css'
-import Video from "frontend-ui/Video"
+import Video from 'frontend-ui/Video'
 import { Img } from '@chakra-ui/react'
-import Carousel from '../../components/Carousel/index'
+import Slider from 'react-slick'
+import 'slick-carousel/slick/slick.css'
+import 'slick-carousel/slick/slick-theme.css'
 
 const Banner = () => {
+  var settings = {
+    dots: true,
+    infinite: true,
+    speed: 500,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    autoplay:true,
+    autoplaySpeed:1000
+  }
   return (
     <>
-      <div className='bannerCarousel'>
-        <ul className='list-none'>
-          <li>  
-            <Img src='https://previews.123rf.com/images/birillo81/birillo811410/birillo81141000048/32895163-turin-torino-panoramic-composition-7000px-x-3000px-.jpg'/>
-          </li>
-          <li>
-            <Video
-              poster="https://f.shgcdn.com/7e53bfca-8e38-4e56-8650-e1d7001544b8/"
-              autoPlay
-            >
-              <source src="https://f.shgcdn.com/147c79f5-1b1a-4428-8b0e-7079cde26003/" />
-            </Video>
-          </li>
-        </ul>
+      <div className="bannerCarousel">
+        <div className="list-none">
+          <Slider {...settings}>
+            <div>
+              <Img src="https://previews.123rf.com/images/birillo81/birillo811410/birillo81141000048/32895163-turin-torino-panoramic-composition-7000px-x-3000px-.jpg" />
+            </div>
+            <div>
+              <Video poster="https://f.shgcdn.com/7e53bfca-8e38-4e56-8650-e1d7001544b8/" autoPlay>
+                <source src="https://f.shgcdn.com/147c79f5-1b1a-4428-8b0e-7079cde26003/" />
+              </Video>
+            </div>
+          </Slider>
+        </div>
       </div>
     </>
   )
