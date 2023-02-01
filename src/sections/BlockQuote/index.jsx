30ce3deb5b351e0a0094ee089101e9e2c8f51862
digitlhaus-dev3 <development3@digitlhaus.com>
2023-02-01 18:35:19 +0530

    Block Quote style

--- src/sections/BlockQuote/index.jsx
+++ src/sections/BlockQuote/index.jsx
@@ -1,12 +1,20 @@
 import React from 'react'
 import styles from './styles.module.css'
 
-const BlockQuote = () => {
-  return (
+const BlockQuote = ({quoteBody,quoteHeading,alignQuote}) => {
+  if(alignQuote?.toLowerCase() === 'left'){
+    alignQuote = 'align-left';
+  }else{
+  alignQuote = 'align-right';
+  }
+  
+   return (
     <>
+    <div className={alignQuote}>
       <div className='block-quote'>
-        <h2 className='text-shadow'>A gaming company for every kind of gamer</h2>
-        <p>We’ve been doing this a while, and it’s safe to say the landscape of gaming has changed immensely over the last decade. Gaming has grown from what some would’ve considered a solo hobby, into a vast and vibrant community. There’s space for everyone here, and the hardware we use should reflect that diversity.</p>
+        <h2 className='text-shadow'>{quoteHeading}</h2>
+        <p>{quoteBody}</p>
+      </div>
       </div>
     </>
   )
--- src/sections/CtaBanner/index.jsx
+++ src/sections/CtaBanner/index.jsx
@@ -1,18 +1,18 @@
 import React from 'react'
 import styles from './styles.module.css'
 
-const CtaBanner = () => {
+const CtaBanner = ({bannerImage, bannerHeading, bannerBody}) => {
   return (
     <>
       <div className='cta-banner'>
         <div className='cta-image'>
-          <img src='https://f.shgcdn.com/8f384e15-cfda-4490-8108-1d5032ca9572/' />
+          <img src={bannerImage?.src} />
         </div>
         <div className='cta-content'>
           <h2 className='cta-heading'>
-            IRONSIDE SERIES
+            {bannerHeading}
           </h2>
-          <p>Our signature prebuilt series featuring classic designs and tried and true performance.</p>
+          <p>{bannerBody}</p>
           <p><a href=''>Learn More</a></p>
         </div>
       </div>
