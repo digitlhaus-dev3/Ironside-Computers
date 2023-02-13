92462acbd4f59916fc72fcd064bc85357a75e603
digitlhaus-dev3 <development3@digitlhaus.com>
2023-02-13 17:20:04 +0530

    Solved Conlicts

--- src/sections/ProductDescription/index.jsx
+++ src/sections/ProductDescription/index.jsx
@@ -1,28 +1,6 @@
-import React from 'react'
-import { useState } from 'react'
-import Image from 'Components/Image'
 import React, { useState, useEffect } from 'react'
 import Image from 'Components/Image'
 
-const ProductDescription = () => {
-  const layout = 'layout1';
-  const [layoutClass,setLayoutClass] = useState();
-  if(layout.toLowerCase() === 'layout1')
-  {
-    setLayoutClass('layout1')
-  }
-  if(layout.toLowerCase() === 'layout2')
-  {
-    setLayoutClass('layout2')
-  }
-  if(layout.toLowerCase() === 'layout3')
-  {
-    setLayoutClass('layout3')
-  }
-  if(layout.toLowerCase() === 'layout4')
-  {
-    setLayoutClass('layout4')
-  }
 const ProductDescription = ({ layout, description, header, image }) => {
   const [layoutClass, setLayoutClass] = useState();
   useEffect(() => {
