eb1e258e50f92ea50b041bce1a8a7ac28f9d40c3
digitlhaus-dev3 <development3@digitlhaus.com>
2023-03-01 17:06:55 +0530

    Added slider card

---
+++ src/sections/SliderCard/index.jsx
@@ -0,0 +1,28 @@
+import React from 'react'
+
+const SliderCard = ({
+  backgroundImage,
+  title,
+  description,
+  price,
+  link,
+  imgSrc,
+  altText,
+  titleColor,
+}) => {
+  return (
+    <div style={{ backgroundImage: `url(${backgroundImage})` }} className="product-card">
+      <div className="product-content">
+        <h3 style={{ color: titleColor }}>{title}</h3>
+        <p>{description}</p>
+        <div className="productCardImage">
+          <p>starts at ${price}</p>
+          <a href={link ? link : '#'}>Learn More</a>
+          <img src={imgSrc} alt={altText} />
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default SliderCard
